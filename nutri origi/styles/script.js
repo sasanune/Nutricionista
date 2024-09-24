ef93809ea83cacc227637a6d78bdfed0d7ab3184
sasanunesw <162514082+sasanune@users.noreply.github.com>
2024-09-23 22:00:14 -0300

    Add files via upload

---
+++ nutri origi/styles/script.js
@@ -0,0 +1,9 @@
+function calculateIMC() {
+    const weight = parseFloat(document.getElementById("weight").value);
+    const height = parseFloat(document.getElementById("height").value);
+    const imc = weight / (height * height);
+    document.getElementById("imcResult").innerText = `Seu IMC é: ${imc.toFixed(
+      2
+    )}`;
+  }
+  
